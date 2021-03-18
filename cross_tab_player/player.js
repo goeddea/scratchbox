0e440a81be9969b201bbeb682c31da4eac4fa6ae
hannes <hannes.waechter@gmail.com>
2021-03-18 22:41:29 +0100

    shared worker basic message exchange

--- cross_tab_player/player.js
+++ cross_tab_player/player.js
@@ -12,95 +12,6 @@ document.body.appendChild(player);
 const tabID = Math.floor(Math.random() * 1000000000);
 console.log("tabId", tabID);
 
-// // broadcasting
-
-// const channel = new BroadcastChannel('player');
-
-// var sendHello = function() {
-
-//     channel.postMessage({
-//         source: tabID,
-//         type: "hello",
-//         helloTime: Date.now()
-//     })
-// }
-// sendHello();
-
-
-//     // basic event broadcasting
-
-// var externalTimeUpdate = false;
-// var onAudioPlayerEvent  = function(event) {
-    
-//     // do not sent a timeupdate event if the audio player event
-//     // was triggered by a broadcast event
-//     if(event.type == "timeupdate" && externalTimeUpdate == true) {
-//         externalTimeUpdate = false;
-//         return;
-//     }
-    
-//     let broadcastContent = {
-//         source: tabID,
-//         type: event.type
-//     };
-//     if(event.type == 'timeupdate') {
-//         broadcastContent.playTime = event.timeStamp;
-//     };
-    
-//     // console.log(event, broadcastContent);
-     
-
-//     channel.postMessage(broadcastContent);
-
-// };
-
-// const eventTypes = ['play', 'pause', 'ended', 'timeupdate'];
-// eventTypes.forEach(eventType => player.addEventListener(eventType, onAudioPlayerEvent))
-
-
-//     // broadcast event handling
-// var updateCounter = 0;
-// var loadTime = Date.now();
-// var timeSinceLoad = 0;
-// var onBroadcastEvent = function(event) {
-//     if(event.data.source == tabID) {
-//         return;
-//     }
-//     // console.log('received broadcast event', event.data.type);
-//     switch (event.data.type) {
-//         case 'timeupdate':
-//             externalTimeUpdate = true;
-//             updateCounter += 1;
-//             timeSinceLoad = Date.now() - loadTime;
-//             // console.log(updateCounter, timeSinceLoad / 1000);
-//             // player.currentTime = event.data.playTime // does not set the (displayed) time
-//             break;
-
-//         case 'hello':
-//             console.log("received 'hello'", event.data.source)
-//             channel.postMessage({
-//                 source: tabID,
-//                 type: 'helloBack',
-//                 target: event.data.source,
-//                 helloTime: event.data.helloTime,
-//                 helloBackTime: Date.now()
-//             })
-//             break;
-        
-//         case 'helloBack':
-//             if(event.data.target == tabID) {
-//                 console.log('received "helloBack" from', 
-//                     event.data.source, "helloReceive", event.data.helloBackTime - event.data.helloTime, "helloBackReceive", Date.now() - event.data.helloBackTime);
-//             }
-//             break;
-            
-//     }
-
-// };
-
-// channel.onmessage = onBroadcastEvent;
-
-
 
 let playButtons = document.getElementsByClassName("js-button-play")
 
@@ -131,8 +42,16 @@ player.addEventListener("pause", function(event) {
 
 // shared worker
 
-var worker = new SharedWorker('sharedworker.js');
-
+var worker = new SharedWorker('sharedworker.js?v=10');
+// worker.port.start(); // this will trigger the on connect event on the webworker
 worker.port.onmessage = function(e) {
-    console.log("worker says", e);
+    console.log("worker says", e.data);
 }
+
+worker.port.postMessage({
+    type: "addTab",
+    target: false,
+    payload: {
+        tabID: tabID,
+    }
+})
--- cross_tab_player/sharedworker.js
+++ cross_tab_player/sharedworker.js
@@ -1,87 +1,105 @@
+
+
+const allPorts = [];
+
+
+var state = {
+    tabs: [],
+    playerTab: null,
+    // focusTab: null,
+    playList: [],
+    playListPosition: null,
+    trackPosition: null,
+    playState: "paused"
+}
+
+
+var addTab = function(e) {
+    let tabID = e.data.payload.tabID;
+    // stretch: check if ID exists, resolve conflict (the two tabs need to re-roll the dice)
+    state.tabs.push(tabID);
+
+    // if is first tab --> maybe initialize as player tab (preload audio file for faster startup)
+
+    let message = {
+        type: "newTab",
+        target: false,
+        payload: {
+            tabID: tabID,
+        },
+    }
+
+    allPorts.forEach(port => {
+      port.postMessage(message);
+    })
+};
+
+var removeTab = function(e) {
+    // check if is player tab --> elect new player tab
+
+    let pos = state.tabs.indexOf(e.tabID);
+    if(pos > -1) {
+        state.tabs.splice(pos, 1);
+    }
+};
+
+var getState = function(e) {
+    port.postMessage({
+        target: e.source,
+        state: state,
+    })
+};
+
+var updatePlaylist = function(e) {
+
+};
+
+var updatePlaylistPosition = function(e) {
+
+};
+
+var updateTrackPosition = function(e) {
+
+};
+
+var updatePlayState = function(e) {
+    // validatey playstate
+    // check if equals current play state
+    state.playState = e.playState;
+    port.postMessage(e.playState);
+};
+
+var onMessage = function(e) {
+    switch (e.data.type) {
+
+        case 'addTab':
+            addTab(e);
+            break;
+        case 'removeTab':
+            removeTab(e);
+            break;
+        case 'getState':
+            getState(e);
+            break;
+        case 'updatePlaylist':
+            updatePlaylist(e);
+            break;
+        case 'updatePlaylistPosition':
+            updatePlaylistPosition(e)
+            break;
+        case 'updateTrackPosition':
+            updateTrackPosition(e)
+            break;
+        case 'updatePlayState':
+            updatePlayState(e)
+            break;
+    }
+}
+
+
 onconnect = function(e) {
-
-    var port = e.ports[0];
-
-    var state = {
-        tabs: [],
-        playerTab: null,
-        // focusTab: null,
-        playList: [],
-        playListPosition: null,
-        trackPosition: null,
-        playState: "paused"
-    }
-
-
-    var addTab = function(e) {
-        // stretch: check if ID exists, resolve conflict (the two tabs need to re-roll the dice)
-        state.tabs.push(e.tabID);
-
-        // if is first tab --> maybe initialize as player tab (preload audio file for faster startup)
-    };
-
-    var removeTab = function(e) {
-        // check if is player tab --> elect new player tab
-
-        let pos = state.tabs.indexOf(e.tabID);
-        if(pos > -1) {
-            state.tabs.splice(pos, 1);
-        }
-    };
-
-    var getState = function(e) {
-        port.postMessage({
-            target: e.source,
-            state: state,
-        })
-    };
-
-    var updatePlaylist = function(e) {
-
-    };
-
-    var updatePlaylistPosition = function(e) {
-
-    };
-
-    var updateTrackPosition = function(e) {
-
-    };
-
-    var updatePlayState = function(e) {
-        // validatey playstate
-        // check if equals current play state
-        state.playState = e.playState;
-        port.postMessage(e.playState);
-    };
-
-    port.onmessage = function(e) {
-        switch (e.type) {
-
-            case 'addTab':
-                addTab(e);
-                break;
-            case 'removeTab':
-                removeTab(e);
-                break;
-            case 'getState':
-                getState(e);
-                break;
-            case 'updatePlaylist':
-                updatePlaylist(e);
-                break;
-            case 'updatePlaylistPosition':
-                updatePlaylistPosition(e)
-                break;
-            case 'updateTrackPosition':
-                updateTrackPosition(e)
-                break;
-            case 'updatePlayState':
-                updatePlayState(e)
-                break;
-        }
-    }
-
-    port.postMessage("hello");
+    let port = e.ports[0];
+    allPorts.push(port);
+    port.onmessage = onMessage; // onmessage setter implicitely calls port.start()
 }
 
